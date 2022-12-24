39b19d27f99cfa41ba8abf68dfd00625b3d2ef06
Samuel Proulx <samuelproulx26@gmail.com>
2022-12-24 17:18:11 -0500

    Go build and format script (#3)
    * Go build and format script
    
    * prettify script
    
    * dependency for max speed

---
+++ ci/go-format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+go_files=$(find ../ScoutAPI -name '*.go')
+
+read -d ' ' -a tokens <<< "$go_files"
+
+for token in "${tokens[@]}"; do
+    result=$(gofmt -l "$token")
+    
+    if [ "$result" != "" ]; then
+        # some files are not formatted properly, exit with error
+        echo "Some files are not formatted properly, please run `go fmt` in /ScoutAPI"
+        exit 1
+    fi
+
+done
+
+exit 0
