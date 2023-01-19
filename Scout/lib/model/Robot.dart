bb17de7fa979e56527cbb28d981d0dd149ec3e12
Samuel Proulx <samuelproulx26@gmail.com>
2023-01-19 10:40:59 -0500

    robot model

--- Scout/lib/model/Robot.dart
+++ Scout/lib/model/Robot.dart
@@ -1,9 +1,12 @@
 import 'package:realm/realm.dart';
 
+part 'Robot.g.dart';
+
 @RealmModel()
 class _Robot {
   @PrimaryKey()
   late final int teamNumber;
+
   late String scoutName;
 
 }
---
+++ Scout/lib/model/Robot.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'Robot.dart';
+
+// **************************************************************************
+// RealmObjectGenerator
+// **************************************************************************
+
+class Robot extends _Robot with RealmEntity, RealmObjectBase, RealmObject {
+  Robot(
+    int teamNumber,
+    String scoutName,
+  ) {
+    RealmObjectBase.set(this, 'teamNumber', teamNumber);
+    RealmObjectBase.set(this, 'scoutName', scoutName);
+  }
+
+  Robot._();
+
+  @override
+  int get teamNumber => RealmObjectBase.get<int>(this, 'teamNumber') as int;
+  @override
+  set teamNumber(int value) => throw RealmUnsupportedSetError();
+
+  @override
+  String get scoutName =>
+      RealmObjectBase.get<String>(this, 'scoutName') as String;
+  @override
+  set scoutName(String value) => RealmObjectBase.set(this, 'scoutName', value);
+
+  @override
+  Stream<RealmObjectChanges<Robot>> get changes =>
+      RealmObjectBase.getChanges<Robot>(this);
+
+  @override
+  Robot freeze() => RealmObjectBase.freezeObject<Robot>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObjectBase.registerFactory(Robot._);
+    return const SchemaObject(ObjectType.realmObject, Robot, 'Robot', [
+      SchemaProperty('teamNumber', RealmPropertyType.int, primaryKey: true),
+      SchemaProperty('scoutName', RealmPropertyType.string),
+    ]);
+  }
+}
