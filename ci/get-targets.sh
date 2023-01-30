d8d4437cfc9aa764ea8ae2abe04f64e1abf2ae49
Samuel Proulx <samuelproulx26@gmail.com>
2023-01-30 11:21:25 -0500

    get targets

--- ci/get-targets.sh
+++ ci/get-targets.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-
 commits=$(git log --format=%H main..$1)
 
+project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
 
-read -d ' ' -a tokens <<< "$commits"
+targets=""
 
-echo "Commits: $commits"
+for commit in $commits; do
+    # find all files that were changed in this commit
+    files=$(git diff-tree --no-commit-id --name-only -r $commit)
+
+    # for each file, check if it is in a target directory
+    for file in $files; do
+        for target in "${project_targets[@]}"; do
+            if [[ $file == $target* ]]; then
+                # append target to targets if it is not already in targets
+                if [[ ! $targets == *"$target"* ]]; then
+                    targets="$targets $target"
+                fi
+            fi
+        done
+    done
+done
+
+echo "TARGETS:: $targets"
