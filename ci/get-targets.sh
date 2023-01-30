ddacf285a7eea72446428f15a65077a96fc46e06
Samuel Proulx <samuelproulx26@gmail.com>
2023-01-30 16:38:06 -0500

    debug git

--- ci/get-targets.sh
+++ ci/get-targets.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 git fetch
+git branch -a
 
-commits=$(git log --format=%H "main..$1")
+# commits=$(git log --format=%H "main..$1")
 
-project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
+# project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
 
-targets=""
+# targets=""
 
-for commit in $commits; do
-    # find all files that were changed in this commit
-    files=$(git diff-tree --no-commit-id --name-only -r $commit)
+# for commit in $commits; do
+#     # find all files that were changed in this commit
+#     files=$(git diff-tree --no-commit-id --name-only -r $commit)
 
-    # # for each file, check if it is in a target directory
-    # for file in $files; do
-    #     for target in "${project_targets[@]}"; do
-    #         if [[ $file == $target* ]]; then
-    #             # append target to targets if it is not already in targets
-    #             if [[ ! $targets == *"$target"* ]]; then
-    #                 targets="$targets $target"
-    #             fi
-    #         fi
-    #     done
-    # done
+#     # # for each file, check if it is in a target directory
+#     # for file in $files; do
+#     #     for target in "${project_targets[@]}"; do
+#     #         if [[ $file == $target* ]]; then
+#     #             # append target to targets if it is not already in targets
+#     #             if [[ ! $targets == *"$target"* ]]; then
+#     #                 targets="$targets $target"
+#     #             fi
+#     #         fi
+#     #     done
+#     # done
 
-    echo "FILES:: $files"
-done
+#     echo "FILES:: $files"
+# done
 
-# echo "TARGETS:: $targets"
+# # echo "TARGETS:: $targets"
