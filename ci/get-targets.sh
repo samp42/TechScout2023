b66cff1c9521dd1086d9a39f7d5cb4b750bd5705
Samuel Proulx <samuelproulx26@gmail.com>
2023-01-30 22:14:55 -0500

    test output

--- ci/get-targets.sh
+++ ci/get-targets.sh
@@ -4,29 +4,25 @@ git fetch
 
 commits=$(git log --format=%H "origin/main..origin/$1")
 
-echo "COMMITS:: $commits"
+project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
 
-# project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
+targets=""
 
-# targets=""
+for commit in $commits; do
+    # find all files that were changed in this commit
+    files=$(git diff-tree --no-commit-id --name-only -r $commit)
 
-# for commit in $commits; do
-#     # find all files that were changed in this commit
-#     files=$(git diff-tree --no-commit-id --name-only -r $commit)
+    # for each file, check if it is in a target directory
+    for file in $files; do
+        for target in "${project_targets[@]}"; do
+            if [[ $file == $target* ]]; then
+                # append target to targets if it is not already in targets
+                if [[ ! $targets == *"$target"* ]]; then
+                    targets="$targets $target"
+                fi
+            fi
+        done
+    done
+done
 
-#     # # for each file, check if it is in a target directory
-#     # for file in $files; do
-#     #     for target in "${project_targets[@]}"; do
-#     #         if [[ $file == $target* ]]; then
-#     #             # append target to targets if it is not already in targets
-#     #             if [[ ! $targets == *"$target"* ]]; then
-#     #                 targets="$targets $target"
-#     #             fi
-#     #         fi
-#     #     done
-#     # done
-
-#     echo "FILES:: $files"
-# done
-
-# # echo "TARGETS:: $targets"
+echo "TARGETS:: $targets"
