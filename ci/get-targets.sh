06fdf87e6368d7c6c68c7d7aee1b999ffa9aa4be
Samuel Proulx <samuelproulx26@gmail.com>
2023-02-02 12:09:46 -0500

    Short ci (#15)
    * git compare
    
    * echo
    
    * script
    
    * indentation
    
    * indentation2
    
    * arg
    
    * other var
    
    * SHEBANG
    
    * get targets
    
    * try to fix
    
    * revision
    
    * revision
    
    * debug
    
    * debug
    
    * fetch
    
    * quotes
    
    * debug git
    
    * debug git 2
    
    * origin
    
    * test output
    
    * retry
    
    * github output
    
    * github output
    
    * targets
    
    * echo
    
    * git fetch
    
    * fix substitution
    
    * output
    
    * use output
    
    * read the doc
    
    * typo
    
    * test script
    
    * test script
    
    * set output
    
    * syntax
    
    * try again
    
    * var
    
    * new script
    
    * needs

---
+++ ci/get-targets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+git fetch
+
+commits=$(git log --format=%H "origin/main..origin/$1")
+
+project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
+
+targets=""
+
+for commit in $commits; do
+    # find all files that were changed in this commit
+    files=$(git diff-tree --no-commit-id --name-only -r $commit)
+
+    # for each file, check if it is in a target directory
+    for file in $files; do
+        for target in "${project_targets[@]}"; do
+            if [[ $file == $target* ]]; then
+                # append target to targets if it is not already in targets
+                if [[ ! $targets == *"$target"* ]]; then
+                    targets="$targets $target"
+                fi
+            fi
+        done
+    done
+done
+
+echo "TARGETS: $targets"
