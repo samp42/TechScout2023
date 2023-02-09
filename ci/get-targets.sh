eaf6a3d121e4dfe3ed45cee9128deec6c72f67fe
Samuel Proulx <samuelproulx26@gmail.com>
2023-02-08 22:55:00 -0500

    fix CI (#19)
    * fix CI'
    
    * conditional linting

--- ci/get-targets.sh
+++ ci/get-targets.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 git fetch
 
-commits=$(git log --format=%H "origin/main..origin/$1")
+branch_name=$1
+branch="${branch_name#*refs/head/}"
 
-project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
+commits=$(git log --format=%H "origin/main..origin/$branch")
 
-targets=""
+# tokenize commits
+read -d ' ' -a commits <<< "$commits"
+
+files=""
 
 for commit in $commits; do
-    # find all files that were changed in this commit
-    files=$(git diff-tree --no-commit-id --name-only -r $commit)
+    commit_files="$(git diff-tree --no-commit-id --name-only -r $commit)"
 
-    # for each file, check if it is in a target directory
-    for file in $files; do
-        for target in "${project_targets[@]}"; do
-            if [[ $file == $target* ]]; then
-                # append target to targets if it is not already in targets
-                if [[ ! $targets == *"$target"* ]]; then
-                    targets="$targets $target"
-                fi
+    for file in $commit_files; do
+        if [[ ! $files == *"$file"* ]]; then
+            files="$files $file"
+        fi
+    done
+done
+
+targets=""
+project_targets=(".github" "ci" "Scout" "ScoutAPI" "ScoutInsights")
+
+for file in $files; do
+    for target in "${project_targets[@]}"; do
+        if [[ $file == $target* ]]; then
+            if [[ ! $targets == *"$target"* ]]; then
+                targets="$targets $target"
             fi
-        done
+        fi
     done
 done
 
