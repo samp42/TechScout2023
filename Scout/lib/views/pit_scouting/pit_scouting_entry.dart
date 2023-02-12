fbe5ae3aaf583d731353670bc9ec464fe305fa8d
Noril Werlen <noril.werlen@pm.me>
2023-02-12 11:38:46 -0500

    add progress bar, resolving issue #13

--- Scout/lib/views/pit_scouting/pit_scouting_entry.dart
+++ Scout/lib/views/pit_scouting/pit_scouting_entry.dart
@@ -11,10 +11,176 @@ class PitScoutingEntry extends StatefulWidget {
 
 class _PitScoutingEntryState extends State<PitScoutingEntry> {
   bool? a = true;
+  int _index = 0;
+  late bool isLastStep;
+
+  List<Step> StepList() => <Step>[
+        Step(
+            title: const Text('Identification'),
+            content: Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Your Name: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Scout Name'),
+                  ))
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Team Number: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter team number'),
+                  ))
+                ]),
+              ),
+            ])),
+        Step(
+            title: const Text('Driver Experience'),
+            content: Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Driver Experience: (years)')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Years',
+                    ),
+                  )),
+                ]),
+              ),
+            ])),
+        Step(
+            title: const Text('Robot infos'),
+            content: Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Weight: (lbs)')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Weight'),
+                  )),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Widht:  (inches)')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'widht'),
+                  )),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Height:  (inches)')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'height'),
+                  )),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Drive Type:  ')),
+                  Expanded(
+                      child: DropDownTextField(dropDownList: [
+                    DropDownValueModel(name: 'Swerve', value: 'Swerve'),
+                    DropDownValueModel(name: 'Tank', value: 'Tank'),
+                    DropDownValueModel(name: 'Other', value: 'Other')
+                  ]))
+                ]),
+              ),
+              Row(children: <Widget>[
+                const Expanded(child: Text('  Is Stable?')),
+                Checkbox(
+                    value: a,
+                    onChanged: (bool? value1) {
+                      setState(() {
+                        a = value1;
+                      });
+                    })
+              ])
+            ])),
+        Step(
+            title: const Text('Game pieces'),
+            content: Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Your Name: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Scout Name'),
+                  ))
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Team Number: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter team number'),
+                  ))
+                ]),
+              ),
+            ])),
+        Step(
+            title: const Text('Scoring'),
+            content: Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Your Name: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Scout Name'),
+                  ))
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Text('  Team Number: ')),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter team number'),
+                  ))
+                ]),
+              ),
+            ])),
+        const Step(
+          title: Text('Charging station'),
+          content: Text('Final Content Review'),
+        ),
+      ];
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController _scrollController = ScrollController();
+    //final ScrollController _scrollController = ScrollController();
     // final driveType = []
 
     return Scaffold(
@@ -24,113 +190,53 @@ class _PitScoutingEntryState extends State<PitScoutingEntry> {
             style: TextStyle(color: yellowT4K),
           ),
         ),
-        body: Column(children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Your Name: ')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Scout Name'),
-              ))
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Team Number: ')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter team number'),
-              ))
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Driver Experience: (years)')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Years',
+        body: Stepper(
+            controlsBuilder: (BuildContext context, ControlsDetails details) {
+              final isLastStep = _index == StepList().length - 1;
+              return Row(children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: details.onStepContinue,
+                    child: (isLastStep)
+                        ? const Text('Submit')
+                        : const Text('Next'),
+                  ),
                 ),
-              )),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Weight: (lbs)')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Weight'),
-              )),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Widht:  (inches)')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'widht'),
-              )),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Height:  (inches)')),
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'height'),
-              )),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: const <Widget>[
-              Expanded(child: Text('  Drive Type:  ')),
-              Expanded(
-                  child: DropDownTextField(dropDownList: [
-                DropDownValueModel(name: 'Swerve', value: 'Swerve'),
-                DropDownValueModel(name: 'Tank', value: 'Tank'),
-                DropDownValueModel(name: 'Other', value: 'Other')
-              ]))
-            ]),
-          ),
-          Row(children: <Widget>[
-            Expanded(child: Text('Is Stable?')),
-            Checkbox(
-                value: a,
-                onChanged: (bool? value1) {
-                  setState(() {
-                    a = value1;
-                  });
-                })
-          ])
-
-          /*   Scrollbar(
-            thumbVisibility: false,
-            controller: _scrollController,
-            child: ListView.builder(
-                controller: _scrollController,
-                itemCount: 2,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Scrollable 1 : Index $index'),
-                  );
-                }),
-          ),*/
-        ]),
+                const SizedBox(
+                  width: 10,
+                ),
+                if (_index > 0)
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: details.onStepCancel,
+                      child: const Text('Back'),
+                    ),
+                  )
+              ]);
+            },
+            currentStep: _index,
+            onStepCancel: () {
+              if (_index > 0) {
+                setState(() {
+                  _index -= 1;
+                });
+              }
+            },
+            onStepContinue: () {
+              if (_index < (StepList().length - 1)) {
+                setState(() {
+                  _index += 1;
+                });
+              } else {
+                print('Submited');
+              }
+            },
+            onStepTapped: (int index) {
+              setState(() {
+                _index = index;
+              });
+            },
+            steps: StepList()),
         resizeToAvoidBottomInset:
             false // the scaffold is not going to resize when open keyboard
         );
