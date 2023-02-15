38115e457f043f73500f36615f3bfe3129ca3000
Samuel Proulx <samuelproulx26@gmail.com>
2023-02-15 13:11:00 -0500

    match model

---
+++ Scout/lib/models/practice_scouting.dart
@@ -0,0 +1,21 @@
+class PracticeScouting {
+  // general info
+  num matchNumber;
+  String scoutName;
+
+  // teams
+  List<num> redTeams;
+  List<num> blueTeams;
+
+  // links
+  num redLinks;
+  num blueLinks;
+
+  // coopertition
+  bool redCoopertition;
+  bool blueCoopertition;
+
+  // endgame
+  bool redEngaged;
+  bool blueEngaged;
+}
